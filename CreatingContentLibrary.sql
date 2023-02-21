84cadd211610122c2ef87e7ae5b929450d80f8ec
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-02-21 09:29:18 +0100

    first commit

---
+++ CreatingContentLibrary.sql
@@ -0,0 +1,27 @@
+USE library;
+INSERT INTO book
+VALUES ("1", "Harry Potter 1");
+
+INSERT INTO book
+VALUES ("2", "Harry Potter 1");
+
+INSERT INTO book
+VALUES ("3", "Harry Potter 2");
+
+INSERT INTO book
+VALUES ("4", "Harry Potter 2");
+
+INSERT INTO customer
+VALUES ("1", "Anna Müller");
+
+INSERT INTO customer
+VALUES ("2", "Karl Schneider");
+
+INSERT INTO customer
+VALUES ("3", "Arno Meier");
+
+INSERT INTO customer
+VALUES ("4", "Maria Dante");
+
+
+
